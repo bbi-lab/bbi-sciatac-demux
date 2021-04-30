99193d7fb8226f85a13a0cebddc937734db6bf34
brgew <bge@uw.edu>
2021-04-29 18:32:59 -0700

    modify run_info.py for NovaSeq v1.5 reagents

--- create_virtual_envs.sh
+++ create_virtual_envs.sh
@@ -22,6 +22,26 @@
 #   o  the pypy virtual environment is used only to run 'barcode_correct_sciatac.py'
 #
 
+
+echo "The virtual environment may depend on the CPU architecture."
+echo "Clusters with mixed node architectures may fail, possibly"
+echo "with Illegal Instruction core dumps when a python script"
+echo "runs in a virtual environment. In this case, you may need" 
+echo "to restrict the hardware resource to the architecture in"
+echo "which the virtual environment was built. If the cluster has" 
+echo "nodes of similar architecture but different generations," 
+echo "one may be able to build the virtual environment on a node" 
+echo "of the earliest generation."
+
+echo
+echo "Press any key to continue."
+echo -n "> "
+while read -r -n1 key
+do
+  echo "Create virtual environment..."
+done
+
+
 #
 # Prepare pypy virtual environment.
 #
